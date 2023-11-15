2d66bf7624bc9e9d47e815e16918397ac6e31424
yskbs <songyang19910315@gmail.com>
2023-11-15 01:07:17 -0500

    Update clean.R

--- clean.R
+++ clean.R
@@ -1,62 +1,59 @@
-library(data.table)
-library(dplyr)
 path <- getwd()
 
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(url, file.path(path, "downloaded.zip"))
-outDir <- file.path(path, "data")
-unzip(zipfile = "downloaded.zip", exdir = outDir)
+filename <- "Coursera_Data.zip"
 
-## check unzipped folder and files
-(list.files(outDir))
-(files <- list.files("UCI HAR Dataset"))
+# Checking if archieve already exists.
+if (!file.exists(filename)){
+  url<- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(url, filename, method="curl")
+  outDir <- file.path(path, "data")
+  unzip(zipfile = "downloaded.zip", exdir = outDir)
+}  
 
+setwd(file.path(path,"data/UCI HAR Dataset"))
 ## read activityLabels and measurements in need
-activityLabels <-fread(
-    file.path(path, "UCI HAR Dataset/activity_labels.txt"),
-    col.names = c("index", "activities")
-  )
-features <- fread(
-  file.path(path, "UCI HAR Dataset/features.txt"),
-  col.names = c("index", "featureNames")
-)
+list.files(getwd())
+
+activityLabels <-fread("activity_labels.txt",col.names = c("index","activities"))
+features <- fread("features.txt",col.names = c("index", "featureNames"))
 head(features)
 featuresneeded <- grep("(mean|std)", features[, featureNames])
 (measurements <- features[featuresneeded, featureNames])
-## remove () and - from measurements, and lowercase them
-measurements <- gsub('[()| -]', '', measurements)
-measurements <- tolower(measurements)
+## remove () from measurements
+measurements <- gsub("[()]", "" , measurements)
 
+
+measurements <- gsub("^t", "Time",measurements)
+measurements <- gsub("^f", "Frequency",measurements)
+measurements <- gsub("Acc", "Accelerometer",measurements)
+measurements <- gsub("arCoeff", "Autorregresioncoefficients",measurements)
+measurements <- gsub("mad", "Medianabsolutedeviation ",measurements)
+measurements <- gsub("sma", "Signalmagnitudearea",measurements)
+measurements <- gsub("Gyro", "Gyroscope",measurements)
+measurements <- gsub("sma", "Signalmagnitudearea",measurements)
 ## read train data
-train <-
-  fread(
-    file.path(path, "UCI HAR Dataset/train/X_train.txt"),
+train <-fread("train/X_train.txt",
     select = featuresneeded,
     col.names = measurements
   )
 
-trainActivities <-
-  fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt"),
+trainActivities <-fread("train/Y_train.txt",
         col.names = c("activity"))
 trainSubjects <-
-  fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt"),
+  fread("train/subject_train.txt",
     col.names = c("subjectnum")
   )
 train <- cbind(trainSubjects, trainActivities,train)
 
 ## read test data
-test <-
-  fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"),
+test <-  fread("test/X_test.txt",
     select = featuresneeded,
     col.names = measurements
   )
 
-testActivities <-
-  fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt")
-        ,
+testActivities <- fread("test/Y_test.txt",
         col.names = c("activity"))
-testSubjects <-
-  fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt"),
+testSubjects <- fread("test/subject_test.txt",
         col.names = c("subjectnum"))
 test <- cbind(testSubjects, testActivities,test)
 
